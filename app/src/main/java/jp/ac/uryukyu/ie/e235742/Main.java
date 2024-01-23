7f4ebdc8b678526107283d6fb362a27938c0a5a9
Daiki Kinjo <e235742@ie.u-ryukyu.ac.jp>
2024-01-23 16:44:51 +0900

    third commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235742/Main.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e235742;
+
+public class Main {
+    public static void main(String[] args){
+        Blackjack blackjackGame = new Blackjack();
+        blackjackGame.StartGame();
+    }
+}
+
