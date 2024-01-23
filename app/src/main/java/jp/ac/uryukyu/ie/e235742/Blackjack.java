f2957fc8a2337f9da52f55331d6c5d9c7798b787
Daiki Kinjo <e235742@ie.u-ryukyu.ac.jp>
2024-01-23 19:16:43 +0900

    force commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235742/Blackjack.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235742/Blackjack.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 
 
 public class Blackjack {
+    private int playerHp = 100;
+    private int playerAttack = 50;
+    private int dealerHp = 100;
+    private int dealerAttack = 50;
     private ArrayList<Integer> deck; //deckのリスト
     private Scanner scanner;
 
@@ -22,9 +26,15 @@ public class Blackjack {
     }
 
     private void playGame() {
+        while (playerHp > 0 && dealerHp > 0) {
+        MakeDeck();
+        System.out.println("ディーラーのHPは残り " + dealerHp + " です");
+        System.out.println("あなたのHPは残り " + playerHp + " です");
+
+        System.out.println("---------------------");
         // プレイヤーの手札
         int handcard1 = deck.get(0); //1枚目取得
-        System.out.println("あなたの1枚目のカードは [" + handcard1 + "] です");
+        System.out.println("\nあなたの1枚目のカードは [" + handcard1 + "] です");
         deck.remove(0);
         int handcard2 = deck.get(0); //2枚目取得
         System.out.println("あなたの2枚目のカードは [" + handcard2 + "] です");
@@ -32,11 +42,9 @@ public class Blackjack {
         int total = (handcard1 > 10 ? 10 : handcard1) + (handcard2 > 10 ? 10 : handcard2); //total計算
         System.out.println("あなたのポイントは [" + total + "] です"); //total表示
 
-        System.out.println("");
-
         //ディーラーの手札
         int dealer_card1 = deck.get(0); //1枚目取得
-        System.out.println("ディーラーの1枚目のカードは [" + dealer_card1 + "] です");
+        System.out.println("\nディーラーの1枚目のカードは [" + dealer_card1 + "] です");
         deck.remove(0);
         int dealer_total = dealer_card1;
         dealer_total = (dealer_card1 > 10 ? 10 : dealer_card1); //total計算 
@@ -46,15 +54,13 @@ public class Blackjack {
     }
 
     while (total < 21) {
-        System.out.println("");
-        System.out.print("Hit（h）かStand（s）を選んでください: ");//ヒットかスタンドか選ばせる
+        System.out.print("\nHit（h）かStand（s）を選んでください: ");//ヒットかスタンドか選ばせる
         String choice = scanner.nextLine();
 
         if (choice.equalsIgnoreCase("h")) {
             // カードを引く
             int newCard = deck.get(0);
-            System.out.println("");
-            System.out.println("新しいカードは [" + newCard + "] です");
+            System.out.println("\n新しいカードは [" + newCard + "] です");
             deck.remove(0);
             total += (newCard > 10 ? 10 : newCard);
             System.out.println("あなたのポイントは [" + total + "] です");
@@ -68,34 +74,62 @@ public class Blackjack {
 
     Judge(total, dealer_total);
     System.out.println("ディーラーのポイントは [" + dealer_total + "] でした");
-    }
+    System.out.println("");
+
+    //Attackturn(total, dealer_total);
+    Finish(playerHp, dealerHp);
+}
+}
 
     private void Judge(int total, int dealer_total){
+        System.out.println("");
         if (total >=22){
             System.out.println("Burst!!");
             System.out.println("You lose.");
+            playerHp -= dealerAttack;
+            System.out.println(dealerAttack + "ダメージ受けた！");
             } else if (dealer_total >= 22){
                 System.out.println("Dealer's Burst!!");
                 System.out.println("You win.");
+                dealerHp -= playerAttack;
+                System.out.println("ディーラーに"+ playerAttack + "ダメージ与えた！");
             } else if (total == 21){
                 System.out.println("BLACK JACK!!!!");
                 System.out.println("You win.");
+                dealerHp -= playerAttack;
+                System.out.println("ディーラーに"+ playerAttack + "ダメージ与えた！");
             } else if (dealer_total == 21){
                 System.out.println("Dealer's BLACK JACK!!!!");
                 System.out.println("You lose.");
+                playerHp -= dealerAttack;
+                System.out.println(dealerAttack + "ダメージ受けた！");
             } else if (total > dealer_total && total < 21 ){
                 System.out.println("You win.");
+                dealerHp -= playerAttack;
+                System.out.println("ディーラーに"+ playerAttack + "ダメージ与えた！");
             } else if (dealer_total > total && dealer_total < 21){
                 System.out.println("You lose.");
+                playerHp -= dealerAttack;
+                System.out.println(dealerAttack + "ダメージ受けた！");
             } else if (total == dealer_total && total < 21){
                 System.out.println("You lose.");
+                playerHp -= dealerAttack;
+                System.out.println(dealerAttack + "ダメージ受けた！");
             }
 
     }
 
+    void Finish(int playerHp, int dealerHp){
+        if (playerHp <= 0){
+            System.out.println("You die.");
+        }else if (dealerHp <= 0){
+            System.out.println("Dealer is dead.");
+        }
+    }
+
     public void StartGame(){
         System.out.println("----- BLACK JACK スタート！ -----");
-        MakeDeck();
+        //MakeDeck();
         playGame();
         scanner.close();
     }
